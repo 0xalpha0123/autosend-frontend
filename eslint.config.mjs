f50e9938888856ebff4510d2dc99f94320959c7b
sp0t <ryan.paliukaitis.my@gmail.com>
2025-01-31 14:05:40 -0500

    fix-type errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,27 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-  ],
-  plugins: [],
-  rules: {
-    // Disable all TypeScript rules
-    '@typescript-eslint/*': 'off'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable TypeScript rules
+      "@typescript-eslint/no-explicit-any": "off",  // Allow the use of 'any'
+      "@typescript-eslint/explicit-module-boundary-types": "off",  // Allow functions without explicit return types
+      "@typescript-eslint/no-unused-vars": "off",  // Turn off unused variable checks
+      "@typescript-eslint/explicit-function-return-type": "off",  // Allow functions without return type
+      "@typescript-eslint/no-non-null-assertion": "off",  // Allow non-null assertions (e.g., `value!`)
+      "@typescript-eslint/ban-ts-comment": "off",  // Allow `@ts-ignore` comments
+    },
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        // You can also add any specific rules for TypeScript files here if needed
-      }
-    }
-  ]
-};
+];
+
+export default eslintConfig;
